2482642b1a1c4c5e01c297c3e36d0d2093576989
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-09 21:42:11 +0900

    Revert "追加の検索条件をコントローラーへ付与"
    This reverts commit 3c3e49f1e430c57b94c0115509823bf897ec126d.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,6 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     set_product_column
-    set_category_column
   end
 
   def search
@@ -19,10 +18,6 @@ class ProductsController < ApplicationController
 
   def set_product_column
     @product_name = Product.select("name").distinct  # 重複なくnameカラムのデータを取り出す
-  end
-
-  def set_category_column
-    @category_name = Category.select("name").distinct
   end
 
 end
